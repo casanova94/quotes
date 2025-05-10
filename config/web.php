cb513e676d51b798aa70eb94b3532729c677f2e4
Luis Casanova <luis.casanova.uady@live.com>
2025-05-09 20:26:05 -0600

    customizing company name

--- config/web.php
+++ config/web.php
@@ -5,6 +5,7 @@ $db = require __DIR__ . '/db.php';
 
 $config = [
     'id' => 'basic',
+    'name' => 'Cotizaciones P.O.E',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
     'language' => 'es-MX',
